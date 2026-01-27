5b484c0fff70c86f88cc94fb5a2b8b20a7d065df
Luke Boyett <lboyett@gmail.com>
2026-01-26 19:04:04 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tradebuilt-web.github.io",
+  base: "/waldvogels-handyman/",
   vite: {
       plugins: [tailwindcss()],
 	},
