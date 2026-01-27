e3d42841c4a57c91264d326000e9c174aa691d87
Luke Boyett <lboyett@gmail.com>
2026-01-26 19:25:37 -0500

    fix links again

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -17,6 +17,10 @@ type MobileMenuProps = {
 
 // TODO: IMPORT PAGES TYPE
 export function MobileMenu({ pages, pathname }: MobileMenuProps) {
+  // TODO: refactor pathname
+  const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
+  const cleanPathname = pathname.replace(basePath, "") || "/";
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -30,21 +34,25 @@ export function MobileMenu({ pages, pathname }: MobileMenuProps) {
       </DropdownMenuTrigger>
       <DropdownMenuContent className={`mr-2`}>
         <DropdownMenuGroup>
-          {pages.map((page) => (
-            <a
-              key={page.title}
-              href={`${import.meta.env.BASE_URL}${page.url.replace(/^\//, "")}`}
-              className={`${
-                pathname === page.url
-                  ? "font-bold"
-                  : "hover:text-gray-500 font-medium"
-              }`}
-            >
-              <DropdownMenuItem className={`text-2xl hover:cursor-pointer`}>
-                {page.title}
-              </DropdownMenuItem>
-            </a>
-          ))}
+          {pages.map((page) => {
+            const href = `${import.meta.env.BASE_URL.replace(/\/$/, "")}${page.url}`;
+
+            return (
+              <a
+                key={page.title}
+                href={href}
+                className={
+                  cleanPathname === page.url
+                    ? "font-bold"
+                    : "hover:text-gray-500 font-medium"
+                }
+              >
+                <DropdownMenuItem className="text-2xl hover:cursor-pointer">
+                  {page.title}
+                </DropdownMenuItem>
+              </a>
+            );
+          })}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
