e884e0d8e75f7bf259f29968bbeb0f15e964cf1d
Luke Boyett <lboyett@gmail.com>
2026-01-29 10:23:34 -0500

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tradebuilt-web.github.io",
-  base: "/waldvogels-handyman/",
+  site: "https://waldvogelshandyman.com",
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
 });
